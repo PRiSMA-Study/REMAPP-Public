156a187829d504029027b5850982ae8727c41bfb
williams_pj@PRIMSA.NET <williams_pj@PRIMSA.NET>
2026-03-26 13:11:53 -0400

    Update outputs

--- ReMAPP-Aim1/3_remapp_aim_one_report_run.R
+++ ReMAPP-Aim1/3_remapp_aim_one_report_run.R
@@ -1,35 +1,17 @@
-<<<<<<< HEAD
 
-
-library(tidyverse)
-library(rmarkdown)
-
-
-UploadDate <- "2025-10-31"  # Define the upload date
-
-output_path <- paste0("D:/Users/williams_pj/Documents/Analysis/ReMAPP/Aim1/",UploadDate , "/ReMAPP_Aim1_Report_", UploadDate ,".pdf")
-
-rmarkdown::render(
-  input = "~/REMAPP-Public/ReMAPP-Aim1/2_ReMAPP_Aim1_Reports_current.Rmd",
-  output_format = "pdf_document",
-  output_file = output_path
-)
-
-=======
 
 
 library(tidyverse)
 library(rmarkdown)
 
 
-UploadDate <- "2025-10-31"  # Define the upload date
+UploadDate <- "2026-01-30"  # Define the upload date
 
 output_path <- paste0("D:/Users/williams_pj/Documents/Analysis/ReMAPP/Aim1/",UploadDate , "/ReMAPP_Aim1_Report_", UploadDate ,".pdf")
 
 rmarkdown::render(
-  input = "~/REMAPP-Public/ReMAPP-Aim1/2_ReMAPP_Aim1_Reports_current.Rmd",
+  input = "~/REMAPP-Public/ReMAPP-Aim1/2_remapp_aim_one_analysis.Rmd",
   output_format = "pdf_document",
   output_file = output_path
 )
 
->>>>>>> 045b86a432752d5e21150935b2d614559ea27c90
